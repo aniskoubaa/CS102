420323d8c0c4491765c420b8593904132243752c
aniskoubaa <akoubaa@coins-lab.org>
2016-01-18 08:52:58 +0300

    commit message

--- src/topic01/files/FileProcessing.java
+++ src/topic01/files/FileProcessing.java
@@ -10,38 +10,54 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class FileProcessing {
-   public static void main(String [] args) throws FileNotFoundException, IOException {
-       
-       String inputFileName = "files/student.txt";
-       String outputFileName = "files/output.txt";
-       
-       //create a file object
-       File inputFile = new File(inputFileName);
-       File outputFile = new File(outputFileName);
+    
+    public static void main(String []args)  {
+    //file name
+    String fileName = "output.txt";
+    
+    
+    //create a file object
+    File file = new File(fileName);
+    
+    //write to a file
+        try{
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write("CS102: We finished the lecture\n");
+            fileWriter.close();
+        }catch (Exception e){
+            System.out.println("your message"+e);
+        }
         
-       //create a scanner object that opens up the file
-       Scanner input = new Scanner(inputFile);
+        //read from a file
+        String inputfileName = "output.txt";
+        File fileToRead = new File(inputfileName);
+        try{
         
-       //create a FileWriter object to write into the file
-       FileWriter fw = new FileWriter (outputFile, false);
- 
-       
-       int count=0;
-       //process the file
-       while (input.hasNext()){
-          String line = input.nextLine(); 
-          count++;
-          String [] lineArray = line.split(":");
-          for (String s : lineArray){
-           System.out.print(s+" ");
-       }
-       System.out.println();
-       }  
-        fw.write("the number of students is " + count+"\n");  
-        fw.close();
-        input.close();
-   }
+        Scanner scanner = new Scanner(fileToRead);
+        
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            System.out.println(line);
+            
+        }
+        }catch(Exception e){
+            System.out.println("I could not open to the file to read.");
+        }
+        
+        
+        
+        
+    
+    
+    }
+  
+    
+    
+    
+    
 }
---
+++ src/topic01/files/TestException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic01.files;
+
+/**
+ *
+ * @author akoubaa
+ */
+public class TestException {
+    public static void main(String []args){
+        int x = 3;
+        int y = 0;
+        
+        System.out.println(x/y);
+    }
+    
+}
