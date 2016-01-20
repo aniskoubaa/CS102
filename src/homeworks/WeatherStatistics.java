35d9b76e24923e60cb67057530123b3dd690cd9d
aniskoubaa <akoubaa@coins-lab.org>
2016-01-20 13:03:54 +0300

    Homework

---
+++ src/homeworks/WeatherStatistics.java
@@ -0,0 +1,31 @@
+/*
+ * Write a Java program that reads the file "files/weather.data"
+ * The program must read the file line by line
+ * The program parses each line that it reads
+ * For example, for this line: 
+ * 19.7|11|1/3/1995
+ * The first value is temperature
+ * The second value is humidity
+ * The last value is the date, composed of day, month, and year 
+ * The program must 
+ * 1- print each line as the following string:
+ *    {"Temperature" = 19.7, "Humidity" = 11, "day"=1, "month" = 3, "year"=1995}
+ * 2- Add all temperature values in an ArrayList temperatureList
+ * 3- Add all humidity values in an ArrayList humidityList
+ * 4- Prints the average temperature as: Average Temperature = avg_temperature_value
+ * 5- Prints the average humidity as: Average Hmudidity = avg_humidity_value
+ * Save the average value of temperature and humidity in a file with name "statistics.txt" in the form
+ * year:temperature average:hmudity average
+ */
+
+
+package homeworks;
+
+public class WeatherStatistics {
+    public static void main(String []args){
+        
+               
+        
+    }
+    
+}
