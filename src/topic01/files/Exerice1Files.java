6b63edf0d70df7f3f591d65bb8e89827765dd938
aniskoubaa <akoubaa@coins-lab.org>
2016-01-20 08:07:58 +0300

    Exercise on Files

---
+++ src/topic01/files/Exerice1Files.java
@@ -0,0 +1,26 @@
+/*
+ * Write a Java Program, that opens the file "files/students.txt"
+ * The program must read the file line by line
+ * The program parses each line that it reads
+ * For example, for this line: 
+ * 1:mohamed:ali:0504123456:cs102:cs202
+ * The program must print
+ *    >ID = 1
+ *    >First Name = Mohamed
+ *    >Last Name = Ali
+ *    >Mobie = 0504123456
+ *    >Courses = cs102, cs202
+ *
+ * In addition, it adds the mobile phone number into an ArrayList called studentPhoneList
+ * Print the content and the size of studentPhoneList
+ */
+package topic01.files;
+
+
+public class Exerice1Files {
+    
+    public static void main(String[]args){
+    
+        
+    }    
+}
