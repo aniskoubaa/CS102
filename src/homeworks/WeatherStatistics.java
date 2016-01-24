c66b487ff486abb664c5ff5d84ab8a5f682d7c3f
aniskoubaa <akoubaa@coins-lab.org>
2016-01-24 13:11:28 +0300

    Homework

--- src/homeworks/WeatherStatistics.java
+++ src/homeworks/WeatherStatistics.java
@@ -21,11 +21,103 @@
 
 package homeworks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class WeatherStatistics {
-    public static void main(String []args) 
-    {
-           
+    
+  public static double getAverage(ArrayList<Double> list){
+      double sum = 0.0;
+      /*
+      for (int i=0;i<list.size();i++){
+          sum =sum + list.get(i);
+      }
+      */
+      //for each statement
+      for (Double d : list){
+          sum += d;
+      }
+      
+      return sum/list.size();
+      
+      
+  }  
+  
+  public static double getAverageInt(ArrayList<Integer> list){
+      double sum = 0.0;
+      
+      //for each statement
+      for (Integer x : list){
+          sum += (double)x;
+      }
+      
+      return sum/list.size();
+      
+      
+  }  
+    
+    
+    public static void main(String []args){
+       
+        
+        ArrayList <Double> temperatureList = new ArrayList<Double>();
+        ArrayList <Integer> humidityList = new ArrayList<Integer>();
+        int year=0;
+        File file = new File("files/weather.txt");
+        Scanner scanner =null;
+        try {
+            scanner = new Scanner (file);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Exception "+ ex);
+        }
+        
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            String [] values = line.split(";|/");
+            
+            double temperature = Double.parseDouble(values[0]);
+            int humidity = Integer.parseInt(values[1]);
+            int day = Integer.parseInt(values[2]);
+            int month = Integer.parseInt(values[3]);
+             year = Integer.parseInt(values[4]);
+            
+            
+            System.out.printf("{\"Temperature\" = %.2f, "
+                    + "\"Humidity\" = %d, "
+                    + "\"day\"=%d, \"month\" = %d, "
+                    + "\"year\"=%d}\n", temperature,
+            humidity, day, month, year);
+            
+            temperatureList.add(temperature);
+            humidityList.add(humidity);            
+        }            
+        
+        double t_avg = getAverage(temperatureList);
+        double h_avg = getAverageInt(humidityList);
+        System.out.println("average of temperature: "+ t_avg);
+        System.out.println("average of humidity: "+h_avg);
+        
+        
+        File fileOutput = new File("files/statistics.txt");
+        //FileWriter fw ;
+        
+      try {
+          FileWriter  fw = new FileWriter(fileOutput);
+          String s = String.format("%d:%.2f:%.2f", year, t_avg, h_avg);
+          fw.write(s);
+          fw.close();
+      } catch (IOException ex) {
+          System.out.println("Exception in FileWriter");
+      }
+        
     }
     
 }
