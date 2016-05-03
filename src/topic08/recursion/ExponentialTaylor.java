c010aa30ff7870035d6e8de3c07ef8d376ef9046
aniskoubaa <akoubaa@coins-lab.org>
2016-05-03 08:58:17 +0300

    recursion

--- src/topic08/recursion/ExponentialTaylor.java
+++ src/topic08/recursion/ExponentialTaylor.java
@@ -13,6 +13,15 @@ public class ExponentialTaylor {
             return eTwo(x,n-1) + Math.pow(x, n)/factorial(n);
         }
     }
+    
+    public static double eTwo1(double x, int n){
+        if(n==0)
+            return Math.E;
+        else {
+            //System.out.println(eTwo(x,n-1));
+            return eTwo(x,n-1) + ((Math.E*Math.pow(x-1, n))/factorial(n));
+        }
+    }
 
     public static double factorial (int n){
         if(n==0)
@@ -25,8 +34,8 @@ public class ExponentialTaylor {
         
         
         for (int i=2;i<3;i++){
-            System.out.println("eTwo("+i+",10): "+eTwo(i,3));
-            //System.out.println("eTwo("+i+",20): "+eTwo(i,20));
+            System.out.println("eTwo("+i+",10): "+eTwo(i,10));
+            System.out.println("eTwo1("+i+",10): "+eTwo1(i,10));
             System.out.println("Math.exp("+i+"): "+Math.exp(i));
             System.out.println("");
         }
